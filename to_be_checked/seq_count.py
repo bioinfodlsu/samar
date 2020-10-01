1af6aa4bd4cd8ef1baaea2f6606e76f3d1f6037c
Anish Shrestha <anish.ms.shrestha@gmail.com>
2020-10-01 11:16:16 +0800

    seq_count.py updated to allow multimapping

--- to_be_checked/seq_count.py
+++ to_be_checked/seq_count.py
@@ -18,19 +18,71 @@ def GetAlignment(p1,p2):
 
     return temp
 
-def main(input_alns, out_counts, multi_mapping):
+def main(input_alns, out_counts, multi_mapping, frag_len_mean, frag_len_std):
 
     final = {}
     lengths = {}
 
-    #upper = frag_len_mean + 2*frag_len_std
-    #lower = frag_len_mean - 2*frag_len_std
 
     if multi_mapping:
-        pass
+        '''
+        1. Because we are only aligning to the (translated) CDS, there could be many cases with only one end aligning. Treat them as if they are one fragment.
+        2. If we do see a concodarnt pair, it's most likely that any other unpaired alignments are of low quality and safe to ignore.
+        '''
+
+        lower = frag_len_mean - 3* frag_len_std
+        upper = frag_len_mean + 3* frag_len_std
+        
+        def update_from_single_end(alns):
+            for aln in alns:
+                ref_id = aln[1]
+                ref_len = aln[5]
+                mismap = float(aln[-1].split("=")[-1])
+                final[ref_id] = final.get(ref_id,0) + (1-mismap)
+                if not ref_id in lengths : lengths[ref_id] = ref_len
+
+        def is_concordant(p1,p2):
+            # 1:ref id, 9: query strand, 2: ref start pos 3: ref aln length
+            if (p1[1] == p2[1] and p1[9] != p2[9]):
+                #print("same chrom, diff strands")
+                if p1[9] == '+' and int(p1[2]) < int(p2[2]):
+                    #print("directionality ok")
+                    frag_len = int(p2[2]) + int(p2[3]) - int(p1[2])
+                    #print(frag_len)
+                    return (lower <= frag_len <= upper)
+                elif p2[9] == '+' and int(p2[2]) < int(p1[2]) :
+                    frag_len = int(p1[2]) + int(p1[3]) - int(p2[2])
+                    #print(frag_len)
+                    return (lower <= frag_len <= upper)
+            #print("False")
+            return False
+
+
+        with open(input_alns) as infile:
+            for key,group in itertools.groupby(FileFilter(infile), lambda x : x[6].rsplit("/",1)[0]):
+
+                block = list(group)
+
+                alns1 = [x for x in block if x[6] == key+"/1"] #alignments of read 1
+                alns2 = [x for x in block if x[6] == key+"/2"] #aligments of read 2
+
+                if len(alns2) == 0:
+                    update_from_single_end(alns1)
+                elif len(alns1) == 0:
+                    update_from_single_end(alns2)
+                else:
+                    #get pairs. ignore singletons. distribute counts using mismap of one of the reads.
+                    for p1 in alns1:
+                        for p2 in alns2:
+                            if is_concordant(p1,p2):
+                                ref_id = p1[1]
+                                ref_len = p1[5]
+                                mismap = float(p1[-1].split("=")[-1])
+                                final[ref_id] = final.get(ref_id,0) + (1-mismap)
+                                if not ref_id in lengths: lengths[ref_id] = ref_len
 
     else:
-        #the assumption here is that there is at most 1 aligment per read,
+        #the assumption here is that there is at most 1 alignment per read,
         #i.e. last-pair-probs was run with -m less than 0.5
 
         with open(input_alns) as infile:
@@ -59,42 +111,6 @@ def main(input_alns, out_counts, multi_mapping):
 
 
 
-
-
-                # #count only concordant pairs for now
-                # concordant_pairs = []
-                # for p1 in pair1:
-                #     for p2 in pair2:
-                #         #print(p1)
-                #         #print(p2)
-
-                #         if is_concordant(p1,p2) :
-                #             concordant_pairs.append(p1) #getting only one of the pairs
-                #
-                # num_of_corcondant_pairs = len(concordant_pairs)
-                # for aln in concordant_pairs:
-                #     ref_id = p1[1]
-                #     ref_len = p1[5]
-                #     final[ref_id] = final.get(ref_id,0) + 1.0/num_of_corcondant_pairs
-                #     if not ref_id in lengths: lengths[ref_id] = ref_len
-                # #print("====")
-                # #Case 1: pair 1 and 2 have exactly 1 alignment
-                # #if((len(pair1) == 1 and len(pair2) == 1) or (len(pair1) == 1 and len(pair2) == 0) or (len(pair1) == 0 and len(pair2) == 1)):
-                # if(len(pair1) == 1 and len(pair2) == 1):
-                #     for alg in block:
-                #         final[alg[1]] = final.get(alg[1],0) + 1
-                # #Case 2: pair 1 or pair 2 has more than 1 alignment and both are not len 0
-                # elif(len(pair1) >= 1 and len(pair2) >= 1):
-                #     temp = []
-                #     for p1 in pair1:
-                #         for p2 in pair2:
-                #             if(p1[1] == p2[1] and lower <= GetAlignment(p1,p2) <= upper):
-                #                 temp.append(p1[1])
-                #
-                #     for ref in temp:
-                #         final[ref] = final.get(ref,0) + (1/len(temp))
-
-
     with open(out_counts,"w") as tab_file:
         writer = csv.writer(tab_file, delimiter='\t')
 
@@ -108,9 +124,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file',help="alignments in tab format")
     parser.add_argument('output_file',help="output file containing counts")
-    parser.add_argument('multi_mapping',action='store_false')
-    #parser.add_argument('frag_len_mean',help="mean of fragment length, when translated. Use last-pair-probs for estimation")
-    #parser.add_argument('frag_len_std',help="standard deviation of fragment length, when translated. Use last-pair-probs for estimation.")
+    parser.add_argument('--multi_mapping',action='store_true')
+    parser.add_argument('--frag_len_mean',type=float,help="mean of fragment length, when translated. Use the same value as last-pair-probs")
+    parser.add_argument('--frag_len_std',type=float,help="standard deviation of fragment length, when translated. Use the same value as last-pair-probs")
 
     args = parser.parse_args()
-    main(args.input_file,args.output_file,args.multi_mapping)
+    main(args.input_file,args.output_file,args.multi_mapping, args.frag_len_mean, args.frag_len_std)
