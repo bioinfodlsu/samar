26a36dce44b59857fc9de8d056cc978543da22d9
zeroh729 <zeroh729@gmail.com>
2021-01-19 10:41:15 +0800

    Added saving of deseq2 RDS file. Added setting of reference group

--- workflow/scripts/last2deseq.R
+++ workflow/scripts/last2deseq.R
@@ -40,10 +40,10 @@ if(sys.nframe() == 0L) {  # mimics __name__ == "main" of python
   opt_parser <- OptionParser(option_list=option_list)
   opt <- parse_args(opt_parser)
 
-  print("--------Passed Arguments---------")
-  for(i in names(opt)){
-    print(paste0(i,": ", opt[[i]]))
-  }
+  # print("--------Passed Arguments---------")
+  # for(i in names(opt)){
+  #   print(paste0(i,": ", opt[[i]]))
+  # }
   
   in_dir <- opt$in_dir
   out_dir <- opt$out_dir # TODO: ask if kyle creates this or I create this
@@ -51,24 +51,55 @@ if(sys.nframe() == 0L) {  # mimics __name__ == "main" of python
   factors <- rjson::fromJSON(opt$factors)
   design <- as.formula(paste0("~", opt$design))
   
-  # TODO : data validation
-  if(is.null(opt$in_dir)){
-    stop("No `in_dir` argument passed. Call --help to see options.")
-  }
+  # ------- Data Validation ------- # 
   # Check if in_dir exists
-  # Check if length of factors is same as length of sample_info rows
-  # Check if all sample file counts exists
+  if(!dir.exists(in_dir)) { 
+    stop("File directory `in_dir` does not exist.")
+  }
   
+  list_files <- list.files(in_dir, recursive = TRUE)
+  n_factors <- length(factors)
+  for(i in names(sample_info)){
+    # Check if length of factors is same as length of sample_info rows
+    if(length(sample_info[[i]]) != n_factors){ 
+      stop(paste0("Sample info of `", i, "` does not have the same length as factors.")) 
+    }
+    
+    # Check if file counts exists
+    file_count <- paste0(i, "/", i, ".counts")
+    if(!file_count %in% list_files){ 
+      stop(paste0("File count `", file_count, "` does not exist in ", in_dir, ".")) 
+    }
+    
+  }
+  
+  # Check if formula contains the factors declared
+  str_design <- strsplit(opt$design, "\\+|\\*")[[1]]
+  str_design <- gsub(" ", "", str_design)
+  for(i in str_design){
+    if(!i %in% factors){
+      stop(paste0("Design contains undeclared factor `", i, "`."))
+    }
+  }
+  
+  # ------- Sample Info Preparation ------- # 
   df_sampleInfo <- data.frame(samples = names(sample_info), do.call(rbind,sample_info), row.names = NULL, stringsAsFactors = TRUE)
   colnames(df_sampleInfo)[-1] <- factors
+  for(i in 2:ncol(df_sampleInfo)){ 
+    # Re-level first sample as reference group 
+    df_sampleInfo[,i] <- relevel(df_sampleInfo[,i], ref = sample_info[[1]][[(i-1)]])
+  }
   
+  # ------- DESeq2 Analysis ------- # 
   deseq <- last2Deseq(in_dir = in_dir, df_sampleInfo = df_sampleInfo, design = design)
   deseq <- DESeq2::DESeq(deseq)
   
-  dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
   
-  write.csv(rowData(deseq), file.path(out_dir, "DESeq2 result.csv"))
-  sink(file.path(out_dir, "DESeq2 result summary.txt"))
+  # ------- Output ------- # 
+  dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
+  saveRDS(deseq, file = file.path(out_dir, "DESeq2 fit.RDS"))
+  write.csv(rowData(deseq), file.path(out_dir, "DESeq2 fit.csv"))
+  sink(file.path(out_dir, "DE count summary.txt"))
   for(r in resultsNames(deseq)[-1]){
     res <- DESeq2::results(deseq, name = r)
     
